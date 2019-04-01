e3d6acadeea5f3044608303d8e4fd9bb330d5ad0
azrap <azraap@gmail.com>
2019-04-01 12:54:21 -0700

    adding function conversions code to function-conversion.js. Did all but the stretch.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,39 @@
 // };
 // myFunction();
 
+// Azra's Code
+let myFunction = () => console.log("Function was invoked!");
+ myFunction();
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+// Azra's Code
+let anotherFunction = (param) => param;
+anotherFunction("Example");
+console.log (anotherFunction("Example")); // console.log to make sure it works
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+// Azra's Code
+let add =(param1, param2) => param1 + param2;
+console.log(add(1,2));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+// Azra's Code
+let subtract =(param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
 // Stretch
 
@@ -28,3 +46,4 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
